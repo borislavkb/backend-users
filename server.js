f760377733f0c183ce414c86815f661c4448dcc1
Borislav <borislavkb@gmail.com>
2021-12-16 22:34:34 +0100

    Server started and listening on port 7000

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 
 const app = express();
+const PORT = 7000; 
 
+//Middleware to parse the body of the request to JSON
 app.use(express.json());
+
+//Endpoints 
+
+app.get("/", (req, res) => {
+    res.json({
+        "/users": "read and create new users", 
+        "/users/:id": "read, update and delete an individual user",
+    })
+});
+
+//Initiate the server
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+});
